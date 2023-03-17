77d18070e8f0c1ba3d1a89da532f5d4556e386b9
Rohithknaidu <121809013+Rohithknaidu@users.noreply.github.com>
2023-03-17 11:06:00 +0530

    Add files via upload

---
+++ forandif.go
@@ -0,0 +1,17 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+	for m:=1;m<100;m++{
+		if m%3==0 && m%5==0{
+			fmt.Printf("%d foo bar\n",m)
+		}else if m%5==0{
+			fmt.Printf("%d bar\n",m)
+		}else if m%3==0{
+			fmt.Printf("%d foo\n",m)
+		}else{
+			fmt.Printf("%d\n",m)
+		}
+	}
+}
